f8ae232e8ef9566b6c7b76a884b7c8744eab18fd
jamesnottidge <freshice57@gmail.com>
2023-09-23 20:33:14 +0100

    server setup

--- test.js
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs/promises';
-
-const readPJSon = async (path) => {
-   const pjsonPath =new URL('./package.json', import.meta.url).pathname;
-    const pjson = await fs.readFile(pjsonPath, 'utf-8');
-    return JSON.parse(pjson);
-}
-
-console.log(await readPJSon());
